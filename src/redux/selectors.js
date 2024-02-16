10dd9373edd4374e4d2384aa6373c4c32a4146fc
Anton <a.chertok@akatech.com.ua>
2024-02-16 12:20:00 +0000

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { filterList } from 'utils/filterList';
 
 export const selectContacts = state => state.contacts.list;
 export const selectFilter = state => state.filter.value;
 
-export const selectFilteredContacts = state =>
-  filterList('name', selectContacts(state), selectFilter(state));
+export const selectContactsLength = state => state.contacts.list.length;
 
-export const selectContactsLength = state => selectContacts(state).length;
+// export const selectFilteredContacts = state =>
+//   filterList('name', selectContacts(state), selectFilter(state));
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => filterList('name', contacts, filter)
+);
